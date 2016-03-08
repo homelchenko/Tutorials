569b8a9d00499bd546f4e43421d6910ea30b6aa8
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-03-08 19:46:25 +0200

    Finished Chapter 2 but it doesn't work with 'directives'

--- Angular2 - Tour of Heroes (ts)/app/app.component.ts
+++ Angular2 - Tour of Heroes (ts)/app/app.component.ts
@@ -1,28 +1,12 @@
 import {Component} from 'angular2/core';
-
-interface Hero {
-    id: number;
-    name: string;
-}
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
     template: ` 
 		<h1>{{title}}</h1>
-		<div *ngIf="selectedHero">
-			<h2>{{selectedHero.name}} details!</h2>
-			<div>
-				<label>id: </label>
-					{{selectedHero.id}}
-			</div>
-			<div>
-				<label>name: </label>
-				<div>
-					<input [(ngModel)]="selectedHero.name" placeholder="name">
-				</div>
-			</div>
-		</div>
-		<h2>My Heroes</h2>
+        <h2>My Heroes</h2>
 		<ul class="heroes">
 			<li *ngFor="#hero of heroes" [class.selected]="hero === selectedHero"  (click)="onSelect(hero)">
 				<span class="badge">{{hero.id}}</span> {{hero.name}}
@@ -72,7 +56,8 @@ interface Hero {
 			margin-right: .8em;
 			border-radius: 4px 0px 0px 4px;
 		}
-	`]
+	`],
+    directives: [HeroDetailComponent],
 })
 export class AppComponent {
     public title = 'Tour of Heroes';
