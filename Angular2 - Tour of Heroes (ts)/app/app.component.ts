d1ae957d7ff63ed449b7f92d9545dc819034c01d
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-03-18 14:28:36 +0200

    Added basic dashboard

--- Angular2 - Tour of Heroes (ts)/app/app.component.ts
+++ Angular2 - Tour of Heroes (ts)/app/app.component.ts
@@ -1,13 +1,18 @@
 import {Component} from "angular2/core";
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
-import {HeroesComponent} from "./heroes.component";
+
 import {HeroService} from "./hero.service";
+import {HeroesComponent} from "./heroes.component";
+import {DashboardComponent} from "./dashboard.component";
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <a [routerLink]="['Heroes']">Heroes</a>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+        </nav>
         <router-outlet></router-outlet>
     `,
     directives: [
@@ -23,6 +28,12 @@ import {HeroService} from "./hero.service";
         path: "/heroes",
         name: 'Heroes',
         component: HeroesComponent   
+    },
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        component: DashboardComponent,
+        useAsDefault: true
     }
 ])
 export class AppComponent{
