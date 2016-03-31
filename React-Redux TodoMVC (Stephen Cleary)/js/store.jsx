f433b89f51ea02fcc444a689591e5079700cc894
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-03-31 18:45:21 +0300

    Added immutability invarants

--- React-Redux TodoMVC (Stephen Cleary)/js/store.jsx
+++ React-Redux TodoMVC (Stephen Cleary)/js/store.jsx
@@ -1,8 +1,11 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import immutableState from 'redux-immutable-state-invariant';
 
 import reducers  from './reducers';
 import DevTools from './devTools';
 
-export default createStore(
-    reducers,
-    DevTools.instrument());
+const middleware = applyMiddleware(immutableState());
+
+const storeFactory = compose(middleware, DevTools.instrument())(createStore);
+
+export default storeFactory(reducers);
