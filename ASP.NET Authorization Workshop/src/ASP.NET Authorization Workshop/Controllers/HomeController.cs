b7110ec96f1051ba4c72884a195ef05850f5dad1
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-04-12 20:42:31 +0300

    Added default ('Home' Controller) and Index action

---
+++ ASP.NET Authorization Workshop/src/ASP.NET Authorization Workshop/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace ASP.NET_Authorization_Workshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
