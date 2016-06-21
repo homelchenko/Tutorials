2fbaf71ae0280ba89405e1a033d27e9a950eb9db
Hennadii Omelchenko <Hennadii Omelchenko>
2016-06-21 18:08:33 +0300

    Added gulp file

--- Working in TypeScript Tutorial (wolksoftware)/gulpfile.js
+++ Working in TypeScript Tutorial (wolksoftware)/gulpfile.js
@@ -19,3 +19,55 @@ gulp.task("lint", function(){
     .pipe(tslint({ }))
     .pipe(tslint.report("verbose"));
 });
+
+var tsProject = tsc.createProject("tsconfig.json");
+
+gulp.task("build-app", function(){
+    return gulp.src([
+        "source/**/**.ts",
+        "typings/main.d.ts/",
+        "source/interfaces/interfaces.d.ts"
+    ])
+    .pipe(tsc(tsProject))
+    .js.pipe(gulp.dest("source/"));
+});
+
+gulp.task("bundle", function(){
+    
+    var libraryName = "myapp";
+    var mainTsFilePath = "source/main.js";
+    var outputFolder = "dist/";
+    var outputFileName = libraryName + ".min.js";
+
+    var bundler = browserify({
+        debug: true,
+        standalone: libraryName
+    });
+
+    return bundler.add(mainTsFilePath)
+        .bundle()
+        .pipe(source(outputFileName))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(outputFolder));
+});
+
+gulp.task("watch", ["default"], function(){
+    
+    browserSync.init({
+        server: "."
+    });
+
+    gulp.watch([
+        "source/**/**.ts",
+        "test/**/*.ts"
+    ],
+    ["default"]);
+
+    gulp.watch("dist/*.js").on('change', browserSync.reload);
+});
+
