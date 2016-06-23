374bfb33fb4930c2275ec65742dc41a6a5120b27
Hennadii Omelchenko <Hennadii Omelchenko>
2016-06-23 14:53:59 +0300

    Got rid of using jQuery dependency

--- Working in TypeScript Tutorial (wolksoftware)/source/main.ts
+++ Working in TypeScript Tutorial (wolksoftware)/source/main.ts
@@ -1,6 +1,8 @@
-import * as $ from "jquery";
 import Greeter from "./entities/greeter";
 
 let greeter = new Greeter("world!");
 let msg = greeter.greet();
-$("body").html(`<h1>${msg}</h1>`);
+
+export default function() {
+    return msg;
+}
