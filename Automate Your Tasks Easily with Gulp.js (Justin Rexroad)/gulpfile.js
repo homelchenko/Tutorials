93cb2abeef40579820a8a64789f4f675d713ba70
Hennadii Omelchenko <Hennadii Omelchenko>
2016-06-25 18:03:03 +0300

    Added jshint and watch tasks

--- Automate Your Tasks Easily with Gulp.js (Justin Rexroad)/gulpfile.js
+++ Automate Your Tasks Easily with Gulp.js (Justin Rexroad)/gulpfile.js
@@ -1,4 +1,15 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint');
+
+gulp.task('jshint', function () {
+    return gulp.src('source/javascript/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+       
+gulp.task('watch', function () {
+    gulp.watch('sources/javascript/**/*.js', ['jshint']);
+});
 
 gulp.task('default', function () {
     // NOP
