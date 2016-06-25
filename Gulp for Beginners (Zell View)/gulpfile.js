4e2cf03ac81f3658554c6160fd59795603e2f1f6
Hennadii Omelchenko <Hennadii Omelchenko>
2016-06-25 14:31:24 +0300

    Defined gulp 'useref' task

--- Gulp for Beginners (Zell View)/gulpfile.js
+++ Gulp for Beginners (Zell View)/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
 
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
@@ -24,4 +25,10 @@ gulp.task('watch', ['browserSync', 'sass'], function () {
 
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('useref', function () {
+    return gulp.src('app/*html')
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
 });
