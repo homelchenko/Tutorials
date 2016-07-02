6ad895c30c5faf09f549db26089d239022b705a4
Hennadii Omelchenko <Hennadii Omelchenko>
2016-07-02 18:13:32 +0300

    Added a new task: 'uglify' to concat and minify js files

--- A Simple Guide to Getting Started With Grunt (Chris Sevilleja)/Gruntfile.js
+++ A Simple Guide to Getting Started With Grunt (Chris Sevilleja)/Gruntfile.js
@@ -12,6 +12,21 @@ module.exports = function (grunt) {
                 'src/**/*.js',
             ],
         },
+
+        uglify: {
+            options: {
+                banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n',
+            },
+
+            build: {
+                files: {
+                    'dist/js/magic.min.js': [
+                        'src/js/magic.js',
+                        'src/js/magic2.js',
+                    ],
+                },
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
