9e0ae438de42815bb79799ca9fb3c4fa4d11d064
Hennadii Omelchenko <Hennadii Omelchenko>
2016-07-02 15:24:13 +0300

    Added a new 'watch' task to track changes of js files in 'app' folder, then 'concat' and 'uglify' are run

--- Grunt for People Who Think Things Like Grunt are Weird and Hard (Chris Coyier)/Gruntfile.js
+++ Grunt for People Who Think Things Like Grunt are Weird and Hard (Chris Coyier)/Gruntfile.js
@@ -32,13 +32,25 @@ module.exports = function (grunt) {
                 }]
             }
         },
+
+        watch: {
+            scripts: {
+                files: ['app/js/**/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: true
+                }
+            }
+        },
     });
 
     // 3. Wher we tell Grunt we plan to use this plug-initConfig
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-
+    
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
     // 4. Where we tell Grunt what to do when we type "grunt' into the termincal
     grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
 };
