c7e3bc2cf1adb7cef1aea3735366f9e712589f62
Hennadii Omelchenko <Hennadii Omelchenko>
2016-07-02 18:25:21 +0300

    Added 'default' task that includes 'jshint', 'uglify', 'cssmin', 'less'

--- A Simple Guide to Getting Started With Grunt (Chris Sevilleja)/Gruntfile.js
+++ A Simple Guide to Getting Started With Grunt (Chris Sevilleja)/Gruntfile.js
@@ -54,4 +54,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'less']);
 };
