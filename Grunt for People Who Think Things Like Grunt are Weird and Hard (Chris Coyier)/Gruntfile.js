272b593bbfdf3bb9d312042f1edbe0028dcb0014
Hennadii Omelchenko <Hennadii Omelchenko>
2016-07-02 13:25:50 +0300

    Added a skeleton for contcatenation work

--- Grunt for People Who Think Things Like Grunt are Weird and Hard (Chris Coyier)/Gruntfile.js
+++ Grunt for People Who Think Things Like Grunt are Weird and Hard (Chris Coyier)/Gruntfile.js
@@ -2,8 +2,16 @@ module.exports = function (grunt) {
     
     // 1. All configuration goes here
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            // 2. Configuration for concatinating files goes here
+        }
     });
 
-    grunt.registerTask('default', []);
+    // 3. Wher we tell Grunt we plan to use this plug-initConfig
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // 4. Where we tell Grunt what to do when we type "grunt' into the termincal
+    grunt.registerTask('default', ['concat']);
 };
