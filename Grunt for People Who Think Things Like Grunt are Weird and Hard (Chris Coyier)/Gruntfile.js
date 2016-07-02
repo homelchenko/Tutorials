a56a15a869fe041e91055d5161393ede036d7719
Hennadii Omelchenko <Hennadii Omelchenko>
2016-07-02 13:19:41 +0300

    Added a skeleton for Grunt file

---
+++ Grunt for People Who Think Things Like Grunt are Weird and Hard (Chris Coyier)/Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function (grunt) {
+    
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    grunt.registerTask('default', []);
+};
