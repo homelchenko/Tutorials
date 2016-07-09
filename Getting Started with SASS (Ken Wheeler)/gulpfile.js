efe8bb5f550ffc0141cc35b621a19ffa94c4f974
Hennadii Omelchenko <Hennadii Omelchenko>
2016-07-09 15:24:01 +0300

    scss & css path moved to constants; Added 'watch' task to track changes in scss files and run 'sass' task

--- Getting Started with SASS (Ken Wheeler)/gulpfile.js
+++ Getting Started with SASS (Ken Wheeler)/gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var scssRootFolder = 'app/scss/**/*.scss';
+var cssDistFolder = 'dist/css';
+
 gulp.task('sass', function () {
-    return gulp.src('app/scss/**/*.scss')
+    return gulp.src(scssRootFolder)
         .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(cssDistFolder));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(scssRootFolder, ['sass']);
 });
 
 gulp.task('default', ['sass']);
