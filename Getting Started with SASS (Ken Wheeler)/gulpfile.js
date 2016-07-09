672e951bf172a290de6951f7d9a0ac758a958cd6
Hennadii Omelchenko <Hennadii Omelchenko>
2016-07-09 19:05:08 +0300

    Added 'copy' task to copy html files to 'dist' folder; Added sample 'index.html' file

--- Getting Started with SASS (Ken Wheeler)/gulpfile.js
+++ Getting Started with SASS (Ken Wheeler)/gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
+// Source constants
 var scssRootFolder = 'app/scss/**/*.scss';
+var htmlRootFolder = 'app/**/*.html';
+
+// Destination contansts
+var distRootFolder = 'dist';
 var cssDistFolder = 'dist/css';
 
 gulp.task('sass', function () {
@@ -13,8 +18,13 @@ gulp.task('sass', function () {
         .pipe(gulp.dest(cssDistFolder));
 });
 
+gulp.task('copy', function () {
+    return gulp.src(htmlRootFolder)
+        .pipe(gulp.dest(distRootFolder))
+});
+
 gulp.task('watch', function () {
     gulp.watch(scssRootFolder, ['sass']);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'copy']);
