3b9b98ead52c4a92fb7762db8f809d9b2c24b03b
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-30 19:46:06 +0300

    Cleans up gulpfile a little bit

--- Developing a Zippy component in Angular 2 (Pascal Precht)/gulpfile.js
+++ Developing a Zippy component in Angular 2 (Pascal Precht)/gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp');
+var typescript = require('gulp-typescript');
+var http = require('http');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var open = require('open');
+var del = require('del');
 
 var PATHS = {
     src: 'src/**/*.ts'
 };
 
 gulp.task('clean', function (done) {
-    var del = require('del');
     del(['dist'], done);
 });
 
 gulp.task('ts2js', function () {
-    var typescript = require('gulp-typescript');
     var tscConfig = require('./tsconfig.json');
-
     var tsResult = gulp
         .src([PATHS.src, 'node_modules/angular2/typings/browser.d.ts'])
         .pipe(typescript(tscConfig.compilerOptions));
@@ -21,11 +24,6 @@ gulp.task('ts2js', function () {
 });
 
 gulp.task('play', ['ts2js'], function () {
-    var http = require('http');
-    var connect = require('connect');
-    var serveStatic = require('serve-static');
-    var open = require('open');
-
     var port = 9000, app;
 
     gulp.watch(PATHS.src, ['ts2js']);
