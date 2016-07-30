bec71f3be28afca8931a2452abd69ff36c8e2a73
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-30 20:04:04 +0300

    Adds Zippy component to the page

--- Developing a Zippy component in Angular 2 (Pascal Precht)/src/hello.ts
+++ Developing a Zippy component in Angular 2 (Pascal Precht)/src/hello.ts
@@ -1,12 +1,16 @@
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 
+import {Zippy} from './zippy';
+
 @Component({
     selector: 'hello-app',
     template: `
         <h1>Hello, {{name}}!</h1>
         Say hello to: <input [value]="name" (input)="name = $event.target.value">
-    `
+        <zippy></zippy>
+    `,
+    directives: [Zippy],
 })
 export class HelloApp {
     name: string = 'World';
