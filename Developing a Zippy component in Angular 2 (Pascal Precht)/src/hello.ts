36351090282b124373a7ba76ec5eeb876292d64a
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-08-06 20:24:13 +0300

    Sample of using Tabs component

--- Developing a Zippy component in Angular 2 (Pascal Precht)/src/hello.ts
+++ Developing a Zippy component in Angular 2 (Pascal Precht)/src/hello.ts
@@ -2,6 +2,8 @@ import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 
 import {ZippyComponent} from './zippy';
+import { Tabs } from './Tabs';
+import { Tab } from './Tab';
 
 @Component({
     selector: 'hello',
@@ -9,8 +11,16 @@ import {ZippyComponent} from './zippy';
         <my-zippy title="Details">
             This is the content
         </my-zippy>
+        <tabs>
+            <tab tabTitle="Foo">
+                Content of tab Foo
+            </tab>
+            <tab tabTitle="Bar">
+                Content of tab Bar
+            </tab>
+        </tabs>
     `,
-    directives: [ZippyComponent],
+    directives: [ZippyComponent, Tabs, Tab],
 })
 export class HelloApp {
 }
