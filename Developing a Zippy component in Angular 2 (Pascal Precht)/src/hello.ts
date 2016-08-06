a520b72c8f4b3d273cadd516c122e5de9ee43971
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-08-06 12:04:24 +0300

    Use new name of Zippy component

--- Developing a Zippy component in Angular 2 (Pascal Precht)/src/hello.ts
+++ Developing a Zippy component in Angular 2 (Pascal Precht)/src/hello.ts
@@ -1,12 +1,12 @@
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 
-import {Zippy} from './zippy';
+import {ZippyComponent} from './zippy';
 
 @Component({
     selector: 'hello',
-    template: '<zippy></zippy>',
-    directives: [Zippy],
+    template: '<my-zippy></my-zippy>',
+    directives: [ZippyComponent],
 })
 export class HelloApp {
 }
