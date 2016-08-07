9f9218e3b6fc702c16191430a763ed0fdf98bcea
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-08-07 19:35:17 +0300

    Fix variable spelling that caused error

--- Working with the PhantomJS Web Page Module (Samuel Oloruntoba)/generator.js
+++ Working with the PhantomJS Web Page Module (Samuel Oloruntoba)/generator.js
@@ -2,7 +2,7 @@ var args = require('system').args;
 var page = require('webpage').create();
 
 var url = args[1];
-var svae = args[2];
+var save = args[2];
 var width = args[3];
 var height = args[4];
 
