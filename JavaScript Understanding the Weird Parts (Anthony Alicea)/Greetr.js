0bee1cce59ca8ad25255bf68a2163f854499627c
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-12-12 17:02:20 +0200

    Expose G$ / Greetr aliases on global object

--- JavaScript Understanding the Weird Parts (Anthony Alicea)/Greetr.js
+++ JavaScript Understanding the Weird Parts (Anthony Alicea)/Greetr.js
@@ -1,3 +1,21 @@
 (function(global, $){
 
+    function Greetr(firstName, lastName, language){
+        return new G$.init(firstName, lastName, language);
+    }
+
+    Greetr.init = function(firstName, lastName, language) {
+        this.firstName = firstName || '';
+        this.lastName = lastName || '';
+        this.language = language || 'en';
+    }
+
+    Greetr.prototype = {
+
+    };
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G$ = Greetr;
+
 }(window, $));
---
+++ JavaScript Understanding the Weird Parts (Anthony Alicea)/app.js
@@ -0,0 +1,4 @@
+var g = G$('John', 'Dow');
+var h = Greetr('Mary', 'Hutson', 'es');
+
+console.log(g, h);
