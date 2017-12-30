571406bd52c0ba3a32ef45dc60ae60ba81df2013
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-12-30 19:14:27 +0200

    Define akward typescript overloads

--- TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
+++ TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
@@ -5,9 +5,19 @@ interface GenerateOptions {
     length: number,
 }
 
-function generateRandomID(options: GenerateOptions): string {
-    return options.symbol + Math.random().toString(36)
-        .substr(2, options.length);
+generateRandomID('#', 8);
+generateRandomID({ symbol: '$', length: 11 });
+
+function generateRandomID(symbol: ValidSymbol, length: number): string;
+function generateRandomID(options: GenerateOptions): string;
+function generateRandomID(optionsOrSymbol: GenerateOptions | ValidSymbol, length?: number): string {
+    if (typeof optionsOrSymbol === 'string') {
+        return optionsOrSymbol + Math.random().toString(36)
+            .substr(2, length);
+    }
+
+    return optionsOrSymbol.symbol + Math.random().toString(36)
+        .substr(2, optionsOrSymbol.length);
 }
 
 function main() {
