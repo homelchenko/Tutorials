2e99d0ea3f609a86e42df8b71980d7e9e9983f73
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-12-30 18:51:52 +0200

    Extract an interface for generation options

--- TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
+++ TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
@@ -1,6 +1,11 @@
 type ValidSymbol = '#' | '$';
 
-function generateRandomID(options: { symbol: ValidSymbol, length: number }): string {
+interface GenerateOptions {
+    symbol: ValidSymbol,
+    length: number,
+}
+
+function generateRandomID(options: GenerateOptions): string {
     return options.symbol + Math.random().toString(36)
         .substr(2, options.length);
 }
