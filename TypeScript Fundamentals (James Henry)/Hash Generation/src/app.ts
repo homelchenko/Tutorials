a44678a680a463cf8ac03ea54b96825fc1cd147a
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-01-02 20:13:31 +0200

    Remove redundant pieces before proceeding

--- TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
+++ TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
@@ -1,14 +1,5 @@
 import { generateRandomID } from './utils';
 
-var id = identity<number | string>(1);
-
-function identity<T>(argument: T): T {
-    return argument;
-}
-
-generateRandomID('#', 8);
-generateRandomID({ symbol: '$', length: 11 });
-
 function main() {
     var appComponent = document.getElementById('app');
     setInterval(function() {
