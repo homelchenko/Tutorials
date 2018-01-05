45debb6ec724c794029caa9acae147b9c3ffe2dc
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-01-05 15:32:31 +0200

    Introduce enumerable decorator factory

--- TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
+++ TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
@@ -1,9 +1,20 @@
 import { generateRandomID, Component } from './utils';
 
+function enumerable(isEnumerable: boolean) {
+    return (
+        target,
+        propertyName,
+        propertyDescriptor: PropertyDescriptor
+    ) => {
+        propertyDescriptor.enumerable = isEnumerable;
+    }
+}
+
 @Component({
     id: 'app'
 })
 class App {
+    @enumerable(false)
     private onInit(element: HTMLElement | null): void {
         if (element) {
             setInterval(function() {
