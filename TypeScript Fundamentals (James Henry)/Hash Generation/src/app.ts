e4b27d69c86497b4854fb044bcb687df8b94d67d
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-01-05 21:24:31 +0200

    Use parameter and property decorators

--- TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
+++ TypeScript Fundamentals (James Henry)/Hash Generation/src/app.ts
@@ -10,12 +10,34 @@ function enumerable(isEnumerable: boolean) {
     }
 }
 
+function prop(){
+    return (
+        target,
+        propertyName
+    ) => {
+        console.log(target, propertyName);
+    }
+}
+
+function param(){
+    return (
+        target,
+        propertyName,
+        index
+    ) => {
+        console.log(target, propertyName, index);
+    }
+}
+
 @Component({
     id: 'app'
 })
 class App {
+    @prop()
+    factor: number;
+
     @enumerable(false)
-    private onInit(element: HTMLElement | null): void {
+    private onInit(@param()element: HTMLElement | null): void {
         if (element) {
             setInterval(function() {
                 element.innerHTML = generateRandomID({
