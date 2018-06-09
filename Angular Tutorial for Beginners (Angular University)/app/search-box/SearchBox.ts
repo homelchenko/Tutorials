0272c770a95d0d7c01cd32386105356745037527
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-06-09 19:22:40 +0300

    Add inline styling for 'reset' button

--- Angular Tutorial for Beginners (Angular University)/app/search-box/SearchBox.ts
+++ Angular Tutorial for Beginners (Angular University)/app/search-box/SearchBox.ts
@@ -5,7 +5,15 @@ declare const module;
 @Component({
     selector: 'search-box',
     moduleId: module.id,
-    templateUrl: 'search-box.component.html'
+    templateUrl: 'search-box.component.html',
+    styles: [`
+        .btn-clear {
+            background: #DE363F;
+            color: white;
+            font-weight: bold;
+            border-radius: 4px;
+        }
+    `]
 })
 export class SearchBox {
     @Input('placeholder')
